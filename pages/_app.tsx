7806e0beffa843333a5795d2e7d768aaef477760
Mati Toledo <toledo.matias@icloud.com>
2022-09-12 12:58:26 -0300

    new analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,25 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import Script from "next/script";
-import Router from "next/router";
-import React, { useEffect } from "react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps, router }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        id="google-tag"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script id="google-analytics" strategy="lazyOnload">
+      <Script id="google-analytics-script" strategy="lazyOnload">
         {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-                });
-            `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+    `}
       </Script>
-
       <Component {...pageProps} />
     </>
   );
